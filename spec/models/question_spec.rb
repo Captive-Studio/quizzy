2060a0cdee6fa16f7fddfc02f581c5246920ff7e
Jean-Franck Ballet <jean-franck@captive.fr>
2013-05-15 15:59:03 +0200

    Ajoute un test qui se trouvait dans qualcomm pour le modèle Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,15 @@ describe Question do
   it { should have_many :choices }
   it { should have_many :translations }
 
+  describe "good_choices" do
+    it "should return the good choices" do
+      choice_1 = Choice.new good: true, text: "choice_1" 
+      choice_2 = Choice.new good: false, text: "choice_2" 
+      choice_3 = Choice.new good: true, text: "choice_3" 
+
+      question = Question.new choices: [choice_1,choice_2,choice_3]
+      question.good_choices.should == [choice_1, choice_3]
+    end
+  end
+
 end
