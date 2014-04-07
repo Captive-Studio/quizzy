4c90c6fbe78093533f17107f1030163ef7e7d9a2
Jean-Franck Ballet <jean-franck@captive.fr>
2014-04-07 16:31:20 +0200

    Ajoute un attribut image à Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 class Question < ActiveRecord::Base
-  attr_accessible :position, :text, :choices_attributes, :choices
+  attr_accessible :position, :text, :choices_attributes, :choices, :image
   validates_presence_of :text
   has_many :choices
   accepts_nested_attributes_for :choices, :allow_destroy => true
 
   default_scope order('position').includes([:choices, :translations])
+
+  mount_uploader :image, Quizzy::ImageUploader
 
   translates :text
 
