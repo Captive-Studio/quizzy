1d7d1da236130051ed33a8d5461cf038606385aa
Remy M <remy@captive.fr>
2014-10-14 12:03:58 +0200

    remplace mauvais require dans lib/quizzy/engine

--- lib/quizzy/engine.rb
+++ lib/quizzy/engine.rb
@@ -2,7 +2,7 @@ module Quizzy
   class Engine < ::Rails::Engine
     isolate_namespace Quizzy
     require "carrierwave"
-    require "globalize3"
+    require "globalize"
     require "mini_magick"
   end
 end
