e67fff7c089c5e570de9e19e5f8c4b088260bac2
Remy M <remy@captive.fr>
2014-10-14 18:02:48 +0200

    modifie les migrations

--- db/migrate/20120416134040_translate_questions.rb
+++ db/migrate/20120416134040_translate_questions.rb
@@ -1,12 +1,12 @@
 class TranslateQuestions < ActiveRecord::Migration
   def self.up
-    Question.create_translation_table!({
+    Quizzy::Question.create_translation_table!({
       :text => :string
     }, {
       :migrate_data => true
     })
   end
   def self.down
-    Question.drop_translation_table! :migrate_data => true
+    Quizzy::Question.drop_translation_table! :migrate_data => true
   end
 end
--- db/migrate/20120416134556_translate_choices.rb
+++ db/migrate/20120416134556_translate_choices.rb
@@ -1,12 +1,12 @@
 class TranslateChoices < ActiveRecord::Migration
   def self.up
-    Choice.create_translation_table!({
+    Quizzy::Choice.create_translation_table!({
       :text => :string
     }, {
       :migrate_data => true
     })
   end
   def self.down
-    Choice.drop_translation_table! :migrate_data => true
+    Quizzy::Choice.drop_translation_table! :migrate_data => true
   end
 end
