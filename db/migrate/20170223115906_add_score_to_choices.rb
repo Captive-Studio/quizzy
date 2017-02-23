2ed04c0e5d4683ad8e5837b12d8e5dae0363a699
cle-captive <clement@captive.fr>
2017-02-23 14:46:58 +0100

    add default to score

--- db/migrate/20170223115906_add_score_to_choices.rb
+++ db/migrate/20170223115906_add_score_to_choices.rb
@@ -1,5 +1,5 @@
 class AddScoreToChoices < ActiveRecord::Migration[5.0]
   def change
-    add_column :quizzy_choices, :score, :integer
+    add_column :quizzy_choices, :score, :integer, default: 0
   end
 end
