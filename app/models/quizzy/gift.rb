8c1d0afc8e5c829ccc0a6e3ea8737d087df2b37d
cle-captive <clement@captive.fr>
2017-11-08 15:56:30 +0100

    changes tabs to spaces

--- app/models/quizzy/gift.rb
+++ app/models/quizzy/gift.rb
@@ -1,9 +1,9 @@
 module Quizzy
-	class Gift < Quizzy::ApplicationRecord
-	  validates_presence_of :attachement, :needed_score
+  class Gift < Quizzy::ApplicationRecord
+    validates_presence_of :attachement, :needed_score
 
-	  mount_uploader :attachement, Quizzy::FileUploader
-	  delegate :url, to: :attachement
+    mount_uploader :attachement, Quizzy::FileUploader
+    delegate :url, to: :attachement
 
-	end
+  end
 end
--- app/models/quizzy/reward.rb
+++ app/models/quizzy/reward.rb
@@ -1,8 +1,8 @@
 module Quizzy
-	class Reward < Quizzy::ApplicationRecord
-	  mount_uploader :image, Quizzy::ImageUploader
+  class Reward < Quizzy::ApplicationRecord
+    mount_uploader :image, Quizzy::ImageUploader
 
-	  belongs_to :participant, class_name: Quizzy.user_class
-	  default_scope { includes([:participant]) }
-	end
+    belongs_to :participant, class_name: Quizzy.user_class
+    default_scope { includes([:participant]) }
+  end
 end
