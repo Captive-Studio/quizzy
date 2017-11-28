03f64010a7d47ad434f7160568fdcd91a435ef3f
cle-captive <clement@captive.fr>
2017-11-28 17:55:32 +0100

    ordering response

--- app/models/quizzy/response.rb
+++ app/models/quizzy/response.rb
@@ -4,7 +4,7 @@ module Quizzy
     has_and_belongs_to_many :choices, class_name: 'Quizzy::Choice'
     validates :question, presence: true
     validates_uniqueness_of :question_id, scope: :quiz_response_id
-    default_scope { includes([:question, :choices]) }
+    default_scope { includes([:question, :choices]).order('quizzy_questions.position') }
 
     def good?
       choices == question.good_choices
@@ -13,6 +13,5 @@ module Quizzy
     def has_made? choice
       choices.include? choice
     end
-    
   end
 end
